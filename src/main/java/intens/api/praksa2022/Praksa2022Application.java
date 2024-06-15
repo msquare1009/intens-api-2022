4c0a87b062b239c8a19bc4a0f6c648307f79cb90
Stimpy <masquare1009@gmail.com>
2024-06-15 15:31:06 +0200

    Check message

--- src/main/java/intens/api/praksa2022/Praksa2022Application.java
+++ src/main/java/intens/api/praksa2022/Praksa2022Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Praksa2022Application {
 
 	public static void main(String[] args) {
+		System.out.println("Revision 0");
 		SpringApplication.run(Praksa2022Application.class, args);
 	}
 
